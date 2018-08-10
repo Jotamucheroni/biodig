6bb8bf7b4411ad734eea1ae18096bd82df0bb13e
Unknown <jotamucheroni@gmail.com>
2018-08-09 23:12:50 -0300

    Solo e tubos de entrada e saida

--- src/biodigestorindiano/Modelo3D.java
+++ src/biodigestorindiano/Modelo3D.java
@@ -70,6 +70,7 @@ public class Modelo3D extends JPanel{
         eixo[0] /= moduloEixo;
         eixo[1] /= moduloEixo;
         eixo[2] /= moduloEixo;
+        
         //Cilindro externo
         vtkImplicitFunction extCilindro = fechaCilindro(centro, eixo, raio, altura);
         
@@ -97,10 +98,18 @@ public class Modelo3D extends JPanel{
 
         //Funções---------------------------------------------------------------
         
+        //Solo
+        vtkImplicitFunction baseSolo     = fechaCilindro(new double[]{0, 2.1, 0}, 
+                                                         new double[]{0, 1, 0}, 
+                                                         4, 0.1),
+                            espacoBioDig = fechaCilindro(new double[]{0, 2.1, 0}, 
+                                                         new double[]{0, 1, 0}, 
+                                                         1.3, 4);
+        
         //Biomassa
-        vtkImplicitFunction cilindroBiomassa = geraCilindro(new double[]{0, 0, 0}, 
-                                                            new double[]{0, 1, 0},
-                                                            1, 2.4, 0.1, 0.2, 0),
+        vtkImplicitFunction cilindroBiomassaNormal = geraCilindro(new double[]{0, 0, 0}, 
+                                                                  new double[]{0, 1, 0},
+                                                                  1, 2.4, 0.1, 0.2, 0),
                             cilindroAux = fechaCilindro(new double[]{0, 0, 0},
                                                         new double[]{0, 1, 0}, 
                                                         0.9, 4);
@@ -120,8 +129,8 @@ public class Modelo3D extends JPanel{
         
         //Parede
         vtkImplicitFunction cilindroParede = fechaCilindro(new double[]{0, -0.5, 0},
-                                                   new double[]{0, 1, 0}, 
-                                                   0.9, 1);
+                                                           new double[]{0, 1, 0}, 
+                                                           0.9, 1);
         vtkPlane corteDir = new vtkPlane(),
                  corteEsq = new vtkPlane();
         
@@ -134,11 +143,78 @@ public class Modelo3D extends JPanel{
             corteEsq.SetNormal(-1, 0, 0);
         }
         
+        //Tubo esquerdo e direito
+        vtkImplicitFunction tuboEsqFuro = new vtkCylinder(),
+                            tuboDirFuro = new vtkCylinder();
+        
+        double    xi         = Math.sin(Math.toRadians(30)),
+                  yi         = Math.cos(Math.toRadians(30)),
+                  Hesq       = 2.15 + 1.2 - 0.2 - 0.3 - 0.1 + 0.5,
+                  compEsq    = Hesq / Math.cos(Math.toRadians(30)),
+                  xEsq       = Math.cos(Math.toRadians(60)) * (compEsq/2),
+                  yEsq       = Math.sin(Math.toRadians(60)) * (compEsq/2),
+                  mEsq       = Math.tan(Math.toRadians(30)) * Hesq,
+                  Hdir       = Hesq - 0.5,
+                  compDir    = Hdir / Math.cos(Math.toRadians(30)),
+                  xDir       = Math.cos(Math.toRadians(60)) * (compDir/2),
+                  yDir       = Math.sin(Math.toRadians(60)) * (compDir/2),
+                  mDir       = Math.tan(Math.toRadians(30)) * Hdir;
+        
+        vtkImplicitFunction tuboEsq = geraCilindro(new double[]{-0.9 - xEsq, -1.2 + 0.2 + 0.3 + 0.1 + yEsq, 0},
+                                  new double[]{-xi, yi, 0}, 
+                                  0.1, compEsq + 0.2, 0.03, 0, 0),
+                            tuboDir = geraCilindro(new double[]{0.9 + xDir, -1.2 + 0.2 + 0.3 + 0.1 + yDir, 0},
+                                   new double[]{xi, yi, 0}, 
+                                   0.1, compDir + 0.2, 0.03, 0, 0);
+        
+        if(tipo == INDIANO)
+        {
+           
+           tuboEsqFuro = fechaCilindro(new double[]{-0.9 - xEsq, -1.2 + 0.2 + 0.3 + 0.1 + yEsq, 0},
+                                       new double[]{-xi, yi, 0}, 
+                                       0.1, compEsq + 0.3);
+           
+           tuboDirFuro = fechaCilindro(new double[]{0.9 + xDir, -1.2 + 0.2 + 0.3 + 0.1 + yDir, 0},
+                                       new double[]{xi, yi, 0}, 
+                                       0.1, compDir + 0.3);
+        }
+        
         //----------------------------------------------------------------------
         
         //Operações-------------------------------------------------------------
         
-        //Biomassa       
+        //Solo
+        vtkImplicitBoolean soloNormal = new vtkImplicitBoolean();
+        soloNormal.SetOperationTypeToDifference();
+        soloNormal.AddFunction(baseSolo);
+        soloNormal.AddFunction(espacoBioDig);
+        
+        vtkImplicitBoolean solo = new vtkImplicitBoolean();
+        solo.SetOperationTypeToDifference();
+        solo.AddFunction(soloNormal);
+        
+        if(tipo == INDIANO)
+        {
+            solo.AddFunction(tuboEsqFuro);
+            solo.AddFunction(tuboDirFuro);
+        }
+        
+        vtkImplicitBoolean corteSolo = new vtkImplicitBoolean();
+        corteSolo.SetOperationTypeToIntersection();
+        corteSolo.AddFunction(solo);
+        corteSolo.AddFunction(corte);
+        
+        //Biomassa
+        vtkImplicitBoolean cilindroBiomassa = new vtkImplicitBoolean();
+        cilindroBiomassa.SetOperationTypeToDifference();
+        cilindroBiomassa.AddFunction(cilindroBiomassaNormal);
+        
+        if(tipo == INDIANO)
+        {
+            cilindroBiomassa.AddFunction(tuboEsqFuro);
+            cilindroBiomassa.AddFunction(tuboDirFuro);
+        }
+        
         vtkImplicitBoolean corteBiomassaCilindro = new vtkImplicitBoolean();
         corteBiomassaCilindro.SetOperationTypeToIntersection();
         corteBiomassaCilindro.AddFunction(cilindroBiomassa);
@@ -163,7 +239,7 @@ public class Modelo3D extends JPanel{
         vtkImplicitBoolean cilindroRevGas = new vtkImplicitBoolean();
         cilindroRevGas.SetOperationTypeToDifference();
         cilindroRevGas.AddFunction(cilindroRevGasometro);
-        cilindroRevGas.AddFunction(cilindroAux); //intCilindro);
+        cilindroRevGas.AddFunction(cilindroAux);
         
         vtkImplicitBoolean corteRevGasCilindro = new vtkImplicitBoolean();
         corteRevGasCilindro.SetOperationTypeToIntersection();
@@ -176,15 +252,36 @@ public class Modelo3D extends JPanel{
         corteGasCilindro.AddFunction(cilindroGasometro);
         corteGasCilindro.AddFunction(corte);
         
+        //Tubo esquerdo e direito
+        vtkImplicitBoolean corteTuboEsq = new vtkImplicitBoolean();
+        vtkImplicitBoolean corteTuboDir = new vtkImplicitBoolean();
+        if(tipo == INDIANO)
+        {
+            corteTuboEsq.SetOperationTypeToIntersection();
+            corteTuboEsq.AddFunction(tuboEsq);
+            corteTuboEsq.AddFunction(corte);
+        
+            corteTuboDir.SetOperationTypeToIntersection();
+            corteTuboDir.AddFunction(tuboDir);
+            corteTuboDir.AddFunction(corte);
+        }
+
         //----------------------------------------------------------------------
         
         //Amostra---------------------------------------------------------------
         
+        //Solo
+        vtkSampleFunction theSoloSample = new vtkSampleFunction();
+        theSoloSample.SetImplicitFunction(solo);
+        theSoloSample.SetModelBounds(-4.1, 4.1, 0, 2.3, -4.1, 4.1);
+        theSoloSample.SetSampleDimensions(80, 80, 80);
+        theSoloSample.ComputeNormalsOff();
+        
         //Biomassa
         vtkSampleFunction theCilindroSample = new vtkSampleFunction();
         theCilindroSample.SetImplicitFunction(cilindroBiomassa);
-        theCilindroSample.SetModelBounds(-1, 1.5, -1.25, 1.25, -1.25, 1.25);
-        theCilindroSample.SetSampleDimensions(100, 100, 100);
+        theCilindroSample.SetModelBounds(-1.1, 1.1, -1.3, 1.3, -1.1, 1.1);
+        theCilindroSample.SetSampleDimensions(80, 80, 80);
         theCilindroSample.ComputeNormalsOff();
         
         //Parede
@@ -192,29 +289,51 @@ public class Modelo3D extends JPanel{
         if(tipo == INDIANO)
         {
             theParedeSample.SetImplicitFunction(parede);
-            theParedeSample.SetModelBounds(-2.3, 2.8, -2.6, 2.6, -2.6, 2.6);
-            theParedeSample.SetSampleDimensions(100, 100, 100);
+            theParedeSample.SetModelBounds(-1.1, 1.1, -1.3, 1.3, -1.1, 1.1);
+            theParedeSample.SetSampleDimensions(80, 80, 80);
             theParedeSample.ComputeNormalsOff();
         }
         
         //Revestimento Gasômetro
         vtkSampleFunction theRevGasCilindroSample = new vtkSampleFunction();
         theRevGasCilindroSample.SetImplicitFunction(cilindroRevGas);
-        theRevGasCilindroSample.SetModelBounds(-2.2, 2.7, -2.5, 2.5, -2.5, 2.5);
-        theRevGasCilindroSample.SetSampleDimensions(100, 100, 100);
+        theRevGasCilindroSample.SetModelBounds(-1.4, 1.4, 1.1, 2.5, -1.4, 1.4);
+        theRevGasCilindroSample.SetSampleDimensions(80, 80, 80);
         theRevGasCilindroSample.ComputeNormalsOff();
         
         //Gasômetro
         vtkSampleFunction theGasCilindroSample = new vtkSampleFunction();
         theGasCilindroSample.SetImplicitFunction(cilindroGasometro);
-        theGasCilindroSample.SetModelBounds(-2.3, 2.8, -2.6, 2.6, -2.6, 2.6);
-        theGasCilindroSample.SetSampleDimensions(100, 100, 100);
+        theGasCilindroSample.SetModelBounds(-1.25, 1.25, 1.1, 2.6, -1.25, 1.25);
+        theGasCilindroSample.SetSampleDimensions(80, 80, 80);
         theGasCilindroSample.ComputeNormalsOff();
+        
+        //Tubo esquerdo e direito
+        vtkSampleFunction theTuboEsqSample = new vtkSampleFunction();
+        vtkSampleFunction theTuboDirSample = new vtkSampleFunction();
+        
+        if(tipo == INDIANO)
+        {
+            theTuboEsqSample.SetImplicitFunction(tuboEsq);
+            theTuboEsqSample.SetModelBounds(-1.1 - mEsq, -0.7, -1.2 + 0.2 + 0.2, 2.15 + 0.7, -0.3, 0.3);
+            theTuboEsqSample.SetSampleDimensions(80, 80, 80);
+            theTuboEsqSample.ComputeNormalsOff();
+            
+            theTuboDirSample.SetImplicitFunction(tuboDir);
+            theTuboDirSample.SetModelBounds(0.7, 1.1 + mDir, -1.2 + 0.2 + 0.2, 2.15 + 0.6, -0.3, 0.3);
+            theTuboDirSample.SetSampleDimensions(80, 80, 80);
+            theTuboDirSample.ComputeNormalsOff();
+        }
         
         //----------------------------------------------------------------------
         
         //Contorno--------------------------------------------------------------
         
+        //Solo
+        vtkContourFilter theSoloSurface = new vtkContourFilter();
+        theSoloSurface.SetInputConnection(theSoloSample.GetOutputPort());
+        theSoloSurface.SetValue(0, 0.0);
+
         //Biomassa
         vtkContourFilter theCilindroSurface = new vtkContourFilter();
         theCilindroSurface.SetInputConnection(theCilindroSample.GetOutputPort());
@@ -235,10 +354,28 @@ public class Modelo3D extends JPanel{
         theGasCilindroSurface.SetInputConnection(theGasCilindroSample.GetOutputPort());
         theGasCilindroSurface.SetValue(0, 0.0);
         
+        //Tubo esquerdo e direito
+        vtkContourFilter theTuboEsqSurface = new vtkContourFilter();
+        vtkContourFilter theTuboDirSurface = new vtkContourFilter();
+        
+        if(tipo == INDIANO)
+        {
+            theTuboEsqSurface.SetInputConnection(theTuboEsqSample.GetOutputPort());
+            theTuboEsqSurface.SetValue(0, 0.0);
+            
+            theTuboDirSurface.SetInputConnection(theTuboDirSample.GetOutputPort());
+            theTuboDirSurface.SetValue(0, 0.0);
+        }
+        
         //----------------------------------------------------------------------
         
         //Polígonos-------------------------------------------------------------
        
+        //Solo
+        vtkPolyDataMapper soloMapper = new vtkPolyDataMapper();
+        soloMapper.SetInputConnection(theSoloSurface.GetOutputPort());
+        soloMapper.ScalarVisibilityOff();
+        
         //Biomassa
         vtkPolyDataMapper cilindroMapper = new vtkPolyDataMapper();
         cilindroMapper.SetInputConnection(theCilindroSurface.GetOutputPort());
@@ -259,10 +396,28 @@ public class Modelo3D extends JPanel{
         gasCilindroMapper.SetInputConnection(theGasCilindroSurface.GetOutputPort());
         gasCilindroMapper.ScalarVisibilityOff();
         
+        //Tubo esquerdo e direito
+        vtkPolyDataMapper tuboEsqMapper = new vtkPolyDataMapper();
+        vtkPolyDataMapper tuboDirMapper = new vtkPolyDataMapper();
+        if(tipo == INDIANO)
+        {
+            tuboEsqMapper.SetInputConnection(theTuboEsqSurface.GetOutputPort());
+            tuboEsqMapper.ScalarVisibilityOff();
+            
+            tuboDirMapper.SetInputConnection(theTuboDirSurface.GetOutputPort());
+            tuboDirMapper.ScalarVisibilityOff();
+        }
+        
         //----------------------------------------------------------------------
         
         //Ator------------------------------------------------------------------
         double[] cor = new double[3];
+        
+        //Solo
+        vtkActor soloActor = new vtkActor();
+        soloActor.SetMapper(soloMapper);
+        new vtkNamedColors().GetColorRGB("Chocolate", cor);
+        soloActor.GetProperty().SetColor(cor);
         
         //Biomassa
         vtkActor cilindroActor = new vtkActor();
@@ -270,10 +425,23 @@ public class Modelo3D extends JPanel{
         new vtkNamedColors().GetColorRGB("Snow", cor);
         cilindroActor.GetProperty().SetColor(cor);
         
-        //Parede
+        //Parede, tubo esquerdo e direito
         vtkActor paredeActor = new vtkActor();
-        paredeActor.SetMapper(paredeMapper);
-        paredeActor.GetProperty().SetColor(cor);
+        vtkActor tuboEsqActor = new vtkActor();
+        vtkActor tuboDirActor = new vtkActor();
+        
+        if(tipo == INDIANO)
+        {
+            paredeActor.SetMapper(paredeMapper);
+            paredeActor.GetProperty().SetColor(cor);
+            
+            tuboEsqActor.SetMapper(tuboEsqMapper);
+            tuboEsqActor.GetProperty().SetColor(cor);
+            
+            tuboDirActor.SetMapper(tuboDirMapper);
+            tuboDirActor.GetProperty().SetColor(cor);
+        }
+        
         
         //Revestimento Gasômetro
         vtkActor revGasCilindroActor = new vtkActor();
@@ -289,9 +457,14 @@ public class Modelo3D extends JPanel{
         //----------------------------------------------------------------------
         
         renWin = new vtkPanel();
+        renWin.GetRenderer().AddActor(soloActor);
         renWin.GetRenderer().AddActor(cilindroActor);
         if(tipo == INDIANO)
+        {
             renWin.GetRenderer().AddActor(paredeActor);
+            renWin.GetRenderer().AddActor(tuboEsqActor);
+            renWin.GetRenderer().AddActor(tuboDirActor);
+        }
         renWin.GetRenderer().AddActor(revGasCilindroActor);
         renWin.GetRenderer().AddActor(gasCilindroActor);
         
@@ -301,8 +474,17 @@ public class Modelo3D extends JPanel{
             vtkImplicitFunction f = cortado ? cilindroBiomassa : corteBiomassaCilindro,
                                 g = cortado ? cilindroRevGas : corteRevGasCilindro,
                                 h = cortado ? cilindroGasometro : corteGasCilindro,
-                                i = cortado ? parede : corteParede;
+                                i = cortado ? parede : corteParede,
+                                j = cortado ? solo : corteSolo,
+                                k = cortado ? tuboEsq : corteTuboEsq,
+                                l = cortado ? tuboDir : corteTuboDir;
             
+            //Solo
+            theSoloSample.SetImplicitFunction(j);
+            theSoloSurface.SetInputConnection(theSoloSample.GetOutputPort());
+            soloMapper.SetInputConnection(theSoloSurface.GetOutputPort());
+            soloActor.SetMapper(soloMapper);
+
             //Biomassa
             theCilindroSample.SetImplicitFunction(f);
             theCilindroSurface.SetInputConnection(theCilindroSample.GetOutputPort());
@@ -329,6 +511,20 @@ public class Modelo3D extends JPanel{
             theGasCilindroSurface.SetInputConnection(theGasCilindroSample.GetOutputPort());
             gasCilindroMapper.SetInputConnection(theGasCilindroSurface.GetOutputPort());
             gasCilindroActor.SetMapper(gasCilindroMapper);
+            
+            //Tubo esquerdo e direito
+            if(tipo == INDIANO)
+            {
+                theTuboEsqSample.SetImplicitFunction(k);
+                theTuboEsqSurface.SetInputConnection(theTuboEsqSample.GetOutputPort());
+                tuboEsqMapper.SetInputConnection(theTuboEsqSurface.GetOutputPort());
+                tuboEsqActor.SetMapper(tuboEsqMapper);
+            
+                theTuboDirSample.SetImplicitFunction(l);
+                theTuboDirSurface.SetInputConnection(theTuboDirSample.GetOutputPort());
+                tuboDirMapper.SetInputConnection(theTuboDirSurface.GetOutputPort());
+                tuboDirActor.SetMapper(tuboDirMapper);
+            }
             
             cortado = !cortado;
             renWin.repaint();
